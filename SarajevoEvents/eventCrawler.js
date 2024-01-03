e07e237c5b95321c7fd064462497ab6f3f681027
iammark <mrdamarkomm@gmail.com>
2024-01-03 14:11:59 +0100

    added two post methods for adding and checking Sarajevo.ba events and some minior changes

--- SarajevoEvents/eventCrawler.js
+++ SarajevoEvents/eventCrawler.js
@@ -2,7 +2,9 @@ const { Builder, By, until } = require('selenium-webdriver');
 require('chromedriver');
 const FileSystem = require('fs');
 
+
 (async function Crawler() {
+    const fetch = await import('node-fetch');
     let driver = await new Builder().forBrowser('chrome').build();
     let list = [];
 
@@ -12,52 +14,66 @@ const FileSystem = require('fs');
       
       console.log("elements:",elements.length)
         for (let i = 0; i < elements.length; i++) {
-            try{
+            
                 let events = {};
 
-                let elementsPostRefresh = []
-                if(i!==0){
-                    elementsPostRefresh = await driver.findElements(By.className('Event_singleEvent__KOwmb'))
-                    let counter = 0
-                    while (elements.length !== elementsPostRefresh.length && counter < 10000) {
-                        counter++;
-                    }
-                    elements = elementsPostRefresh
-                }
-
+                
+                elements = await driver.findElements(By.className('Event_singleEvent__KOwmb'));
                 const titleElement = await elements[i].findElement(By.className('Event_selectedEventText__tLPSX'));
                 const title = await titleElement.getText();
                 events = { ...events, title };
                 driver.executeScript("arguments[0].scrollIntoView(true);", titleElement)
 
-                const imageElement = await elements[i].findElement(By.css('img'));
-                const srcValue = await imageElement.getAttribute('src');
-                events = { ...events, image: [srcValue] };
+                
 
                 const locationElement = await elements[i].findElement(By.className('Event_single-event-location-name___E_K8'))
-                const locationValue = await locationElement.getText();
-                events = {...events, locationValue}
+                const location = await locationElement.getText();
+                events = {...events, location}
                 
                 const timeElement = await elements[i].findElement(By.className('Event_single-event-time__NrhNZ'))
-                const timeValue = await timeElement.getText();
-                events = {...events,timeValue}
+                const startTime = await timeElement.getText();
+                events = {...events,startTime}
+                
+                const existTemp = await fetch.default(
+                    'http://127.0.0.1:5001/maptobe-dev/us-central1/app/v1/events/checksSarajevoEvents',{
+                        method:'POST',
+                        body: JSON.stringify({
+                        title:title,
+                        startTime:startTime
 
+                    }),headers:{
+                        'Content-Type': 'application/json'
+                    },
+                    redirect: 'follow'
+                }
+                )
+                const exist = await existTemp.json()
+                
+                if(exist){
+                    console.log('Skipping event as it already exists:', title, startTime);
+                    continue;
+                }
+                
                 elements[i].click()
                 await driver.sleep(2000)
                 
                 await driver.wait(until.elementLocated(By.className('Event_modal-information-price__qzKsX css-1ngehnn')), 5000);
                 
+                const findSrcElement = await driver.findElement(By.className('Event_modal-image-container__O_Qfi'))
+                const imageElement = await findSrcElement.findElement(By.css('img'));
+                const images = await imageElement.getAttribute('src');
+                events = { ...events, image: [images] };
+                
+
                 const priceElement = await driver.findElement(By.className('Event_modal-information-price__qzKsX css-1ngehnn'))
                 const priceValue = await priceElement.getText();
-                console.log("line 44")
+                
                 events = {...events,priceValue}
                 const descriptionElement = await driver.findElement(By.className('chakra-text Event_modal-body-text__jHthh css-0'))
-                const descriptionValue = await descriptionElement.getText();
+                const description = await descriptionElement.getText();
                 
-                events = {...events,descriptionValue}
-                console.log('price',priceValue)
-                console.log('desc',descriptionValue)
-                console.log("line 52")
+                events = {...events,description}
+                console.log(events)
                 
                 
                 
@@ -65,22 +81,35 @@ const FileSystem = require('fs');
                 list.push(events);
                 await driver.sleep(1500)
             
-            }catch(error){     
-                console.log(i)
-                throw error;
-            }
+                
+
+                fetch.default(
+                    
+                    "http://127.0.0.1:5001/maptobe-dev/us-central1/app/v1/events/addSarajevoEvents",
+                    {
+                      method: 'POST',
+                      body: JSON.stringify(events),
+                      headers: {
+                        'Content-Type': 'application/json'
+                      },
+                      redirect: 'follow'
+                    })
+                    .then(response => response.text())
+                    .then(result =>
+                    {
+                      console.log('Recorded data: ', result)
+                    })
+                    .catch(error =>
+                    {
+                      console.log('Error recording data', error, JSON.stringify(error))
+                      setSending(false)
+                    });
+                    list.push(events)
+            
         }
         
 
-        // Writing json file with collected data
-        const jsonContent = JSON.stringify(list);
-        FileSystem.writeFile('./sarajevoEventsData.json', jsonContent, 'utf8', function (err) {
-            if (err) {
-                //console.log('An error occurred while writing JSON Object to File.');
-                return //console.log(err);
-            }
-            //console.log('JSON file has been saved!');
-        });
+        
     } finally {
         await driver.quit();
     }
